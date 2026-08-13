5d6d1189d2e78e2bd56d94e588167a9eb1da2f2d
MeghajBUD <jamgem365@gmail.com>
2026-08-13 18:13:49 -0400

    untrack build artifacts; apply gitignore

--- Meghaj_roboracer_ws/build/f1tenth_gym_ros/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/orin/Desktop/roboracer-purdue/Meghaj_roboracer_ws/install/f1tenth_gym_ros'
--- Meghaj_roboracer_ws/build/f1tenth_stack/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/orin/Desktop/roboracer-purdue/Meghaj_roboracer_ws/install/ackermann_mux/share/ackermann_mux/package.sh"
