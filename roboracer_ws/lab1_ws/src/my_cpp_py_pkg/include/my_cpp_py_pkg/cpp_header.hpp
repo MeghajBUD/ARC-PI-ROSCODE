62cd2b4cf7beea447482af8ec127c13d4a338382
MeghajBUD <jamgem365@gmail.com>
2026-08-14 15:50:43 -0400

    Added lab 1 template

--- roboracer_ws/lab1_ws/src/my_cpp_py_pkg/include/my_cpp_py_pkg/cpp_header.hpp
+++
@@ -1,7 +0,0 @@
-// include/my_cpp_py_pkg/cpp_header.hpp
-#ifndef MY_CPP_PY_PKG__CPP_HEADER_HPP_
-#define MY_CPP_PY_PKG__CPP_HEADER_HPP_
-
-#include "rclcpp/rclcpp.hpp"
-
-#endif  // MY_CPP_PY_PKG__CPP_HEADER_HPP_
