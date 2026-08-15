69cc36dd884ad8c75447a64a3cdcf79b83939185
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2026-08-15 15:34:40 +0000

    new dev container files

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 set -e
 
-# Source ROS setup
-source /opt/ros/${ROS_DISTRO:-noetic}/setup.bash
+# Source ROS 2
+source /opt/ros/humble/setup.bash
 
 # Install workspace dependencies using rosdep
 cd /workspaces/roboracer-purdue
 rosdep install --from-paths src --ignore-src -r -y
 
-# Build the workspace (choose the appropriate build tool)
-# For catkin (ROS1):
-catkin_make
-# For catkin_tools:
-# catkin build
-# For colcon (ROS2):
-# colcon build
+# Build the workspace with colcon (ROS 2)
+colcon build --symlink-install
 
 # Source the workspace to verify
-source devel/setup.bash   # for catkin
-# source install/setup.bash   for colcon
+source install/setup.bash
 
 echo "Workspace built successfully!"
