b1bac99abe309ac4baeae9a9b2b888ddc1ab211c
Maninder Kaur <75804314+k4urman@users.noreply.github.com>
2026-08-15 16:01:00 +0000

    devcontainer test #4

--- .devcontainer/start-vnc.sh
+++ .devcontainer/start-vnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Start tightvncserver on display :1
+# Start VNC server on display :1
 vncserver :1 -geometry 1280x720 -depth 24 -localhost no
 
-# Start websockify to bridge VNC to web (noVNC)
-websockify --web /usr/share/novnc 6080 localhost:5901
+# Start websockify in the background so the script can exit
+websockify --web /usr/share/novnc 6080 localhost:5901 &
